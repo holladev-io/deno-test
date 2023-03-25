93ae1d81b1a856955e43e7af5127922eac24c5ea
Holla Dev <holladev.io@gmail.com>
2023-03-25 09:14:03 +0200

    first commit

---
+++ main.ts
@@ -0,0 +1,20 @@
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+
+const router = new Router();
+router.get("/", (ctx) => {
+  ctx.response.body = `<!DOCTYPE html>
+    <html>
+      <head><title>Hello oak!</title><head>
+      <body>
+        <h1>Hello oak!</h1>
+      </body>
+    </html>
+  `;
+});
+
+const app = new Application();
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+console.log("Listen on http://localhost:8080")
+app.listen({ port: 8080 });
